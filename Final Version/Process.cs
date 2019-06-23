f6738af78b5cf5fe51f3761ade0af6cfbf05c5b1
ahmedtarek97 <36510613+ahmedtarek97@users.noreply.github.com>
2019-06-23 19:53:57 +0200

    Add files via upload

---
+++ Final Version/Process.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryAllocation
+{
+    class Process
+    {
+        private string name = "";
+        bool fit = true;
+        bool allocated = false;
+        public LinkedList<hole> segments=new LinkedList<hole>() ;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+
+                name = value;
+
+            }
+        }
+        public bool Fit
+        {
+            get
+            {
+                return fit;
+            }
+            set
+            {
+
+                fit = value;
+
+            }
+        }
+        public bool Allocated
+        {
+            get
+            {
+                return allocated;
+            }
+            set
+            {
+
+                allocated = value;
+
+            }
+        }
+
+    }
+}
